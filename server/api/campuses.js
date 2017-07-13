0a1f7d457f4b018d8d738b8099509266b04261c8
Ken Russo <krrusso@gmail.com>
2017-07-13 17:11:13 -0400

    all but put working

--- server/api/campuses.js
+++ server/api/campuses.js
@@ -1,5 +1,17 @@
 const router = require("express").Router();
 const Campus = require("../../db/models/index").Campus;
+// var HttpError = require('../../utils/HttpError');
+
+router.param("id", (req, res, next, id) => {
+  Campus.findById(id)
+    .then(campus => {
+      // if (!campus) throw HttpError(404);
+      req.requestedCampus = campus;
+      next();
+      return null;
+    })
+    .catch(next);
+});
 
 router.get("/", (req, res, next) => {
   console.log("----------------=-=-=-=-=", Campus);
@@ -14,6 +26,18 @@ router.get("/:id", (req, res, next) => {
 
 router.post("/", (req, res, next) => {
   Campus.create(req.body).then(dbRes => res.json(dbRes)).catch(next);
+});
+
+router.delete("/:id", (req, res, next) => {
+  req.requestedCampus
+    .destroy()
+    .then(() => {
+      res.status(204).end();
+    })
+    .then(() => {
+      res.json("------you have deleted a campus!------");
+    })
+    .catch(next);
 });
 
 module.exports = router;
--- server/api/students.js
+++ server/api/students.js
@@ -1,5 +1,17 @@
 const router = require("express").Router();
 const Student = require("../../db/models/index").Student;
+// if (!student) throw HttpError(404);
+
+router.param("id", function(req, res, next, id) {
+  Student.findById(id)
+    .then(function(student) {
+      // if (!student) throw HttpError(404);
+      req.requestedStudent = student;
+      next();
+      return null;
+    })
+    .catch(next);
+});
 
 router.get("/", (req, res, next) => {
   console.log("----------------=-=-=-=-=", Student);
@@ -7,13 +19,25 @@ router.get("/", (req, res, next) => {
 });
 
 router.get("/:id", (req, res, next) => {
-  Student.findById(req.params.id).then(student => {
+  Student.findById(req.requestedStudent).then(student => {
     res.json(student).catch(next);
   });
 });
 
 router.post("/", (req, res, next) => {
   Student.create(req.body).then(students => res.json(students)).catch(next);
+});
+
+router.delete("/:id", (req, res, next) => {
+  req.requestedStudent
+    .destroy()
+    .then(() => {
+      res.status(204).end();
+    })
+    .then(() => {
+      res.json("=-=-=-=you have deleted someone...-=-=-=-=");
+    })
+    .catch(next);
 });
 
 module.exports = router;
